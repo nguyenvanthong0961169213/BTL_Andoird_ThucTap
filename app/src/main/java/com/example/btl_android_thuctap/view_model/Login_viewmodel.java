6daf6fe82069a5afe1e3fab66390c06f5fd57d00
Thong_Nguyen <nguyenvanthong0961169213@gmail.com>
2022-05-02 13:44:05 +0700

    'tiep'

--- app/src/main/java/com/example/btl_android_thuctap/view_model/Login_viewmodel.java
+++ app/src/main/java/com/example/btl_android_thuctap/view_model/Login_viewmodel.java
@@ -1,4 +1,6 @@
-package com.example.btl_android_thuctap.ViewModel;
+package com.example.btl_android_thuctap.view_model;
+
+import android.view.View;
 
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
@@ -7,11 +9,12 @@ import androidx.databinding.ObservableField;
 import com.example.btl_android_thuctap.BR;
 import com.example.btl_android_thuctap.Model.login_user;
 
-public class Login_viewmodel extends BaseObservable {
+public class Login_viewmodel extends BaseObservable{
     //Attributes
     private String PhoneNumber;
     private String Password;
-    public ObservableField<String> validate=new ObservableField<>();
+    public ObservableField<String> validate=new ObservableField<>("kaka");
+
 
     //Getters and Setter
     @Bindable
@@ -35,10 +38,13 @@ public class Login_viewmodel extends BaseObservable {
 
     public void Onclick()
     {
-        login_user user=new login_user(getPhoneNumber(),getPassword());
-        if(user.validatephonenumber()==false||user.validatevapassword()==false)
-        {
-            validate.set("Lỗi Tài Khoản và mật khẩu");
+        login_user user = new login_user(getPhoneNumber(),getPassword());
+        setPhoneNumber("1111");
+        if(user.validatevapassword() && user.validatephonenumber()){
+            validate.set("thanh cong");
+        }else {
+            validate.set("that bai");
         }
     }
+
 }
