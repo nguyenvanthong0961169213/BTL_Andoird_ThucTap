32d9f31d458540c8e8ad3bcc9daac0314a1bac03
buihieu2001 <hieuomo1311@gmail.com>
2022-05-03 17:29:57 +0500

    Hieu

--- app/src/main/java/com/example/bt_android_thuctap/SignInFragment.java
+++ app/src/main/java/com/example/bt_android_thuctap/SignInFragment.java
@@ -2,11 +2,15 @@ package com.example.bt_android_thuctap;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.bt_android_thuctap.databinding.FragmentSignInBinding;
+import com.example.bt_android_thuctap.viewmodel.LoginViewModel;
 
 public class SignInFragment extends Fragment {
 
@@ -26,7 +30,11 @@ public class SignInFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        FragmentSignInBinding fragmentSignInBinding = DataBindingUtil.setContentView(getActivity(),
+                R.layout.fragment_sign_in);
+        LoginViewModel loginViewModel = new LoginViewModel();
+        fragmentSignInBinding.setLoginViewModel(loginViewModel);
+        return view;
     }
 }
